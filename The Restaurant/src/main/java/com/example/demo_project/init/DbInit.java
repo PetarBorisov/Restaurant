1e1faa574057a39ed7b25a8fe0cf070cfca0b9fe
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-10-30 15:33:04 +0200

    Starting

---
+++ The Restaurant/src/main/java/com/example/demo_project/init/DbInit.java
@@ -0,0 +1,19 @@
+package com.example.demo_project.init;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DbInit implements CommandLineRunner {
+    private final String defaultAdminPass;
+
+    public DbInit(@Value("${restaurant.default.admin.pass}") String defaultAdminPass) {
+        this.defaultAdminPass = defaultAdminPass;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //TODO
+    }
+}
