fad44c0f6f7f8fbe48d7ec849409f533603a23c3
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-01 00:30:52 +0200

    Create Menu.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/model/entity/Menu.java
@@ -0,0 +1,44 @@
+package com.example.demo_project.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name = "menu")
+public class Menu extends BaseEntity {
+
+    @OneToMany(mappedBy = "menu")
+    private List<DrinkEntity> drinkEntities;
+
+    @OneToMany(mappedBy = "menu")
+    private List<LunchEntity> lunchEntities;
+
+    @OneToMany(mappedBy = "menu")
+    private List<DinnerEntity> dinnerEntities;
+
+    public List<DrinkEntity> getDrinkEntities() {
+        return drinkEntities;
+    }
+
+    public void setDrinkEntities(List<DrinkEntity> drinkEntities) {
+        this.drinkEntities = drinkEntities;
+    }
+
+    public List<LunchEntity> getLunchEntities() {
+        return lunchEntities;
+    }
+
+    public void setLunchEntities(List<LunchEntity> lunchEntities) {
+        this.lunchEntities = lunchEntities;
+    }
+
+    public List<DinnerEntity> getDinnerEntities() {
+        return dinnerEntities;
+    }
+
+    public void setDinnerEntities(List<DinnerEntity> dinnerEntities) {
+        this.dinnerEntities = dinnerEntities;
+    }
+}
