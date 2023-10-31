e1a9c9fb6b7e4534eaf2cb727754394a49b1ad44
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-01 00:30:49 +0200

    Create LunchEntity.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/model/entity/LunchEntity.java
@@ -0,0 +1,71 @@
+package com.example.demo_project.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "lunches")
+public class LunchEntity extends BaseEntity{
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String photo;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    private Menu menu;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public LunchEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LunchEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public LunchEntity setPhoto(String photo) {
+        this.photo = photo;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public LunchEntity setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public LunchEntity setMenu(Menu menu) {
+        this.menu = menu;
+        return this;
+    }
+}
