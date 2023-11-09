b8f1426854aba7af68f91dcc9ed5776819aea60b
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-10 01:05:57 +0200

    update database

---
+++ The Restaurant/src/main/java/com/example/demo_project/model/entity/ReservationEntity.java
@@ -0,0 +1,91 @@
+package com.example.demo_project.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "reservations")
+public class ReservationEntity extends BaseEntity{
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer phoneNumber;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime time;
+
+    @Column(nullable = false)
+    private Integer numberPersons;
+
+    @ManyToOne
+    private UserEntity user;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public Integer getNumberPersons() {
+        return numberPersons;
+    }
+
+    public void setNumberPersons(Integer numberPersons) {
+        this.numberPersons = numberPersons;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+}
---
+++ The Restaurant/src/main/java/com/example/demo_project/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo_project.repository;
+
+import com.example.demo_project.model.entity.ReservationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
+
+}
