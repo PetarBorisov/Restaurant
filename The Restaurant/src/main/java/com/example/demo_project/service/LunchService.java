ecb1019b9ebb2043dead9f1a5322628b139b3a11
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-09 20:13:30 +0200

    Create LunchService.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/service/LunchService.java
@@ -0,0 +1,22 @@
+package com.example.demo_project.service;
+
+import com.example.demo_project.model.dto.LunchAddDTO;
+import com.example.demo_project.model.dto.LunchEditDTO;
+import com.example.demo_project.model.entity.LunchEntity;
+
+import java.util.List;
+
+public interface LunchService {
+
+
+    List<LunchEntity> getAllLunches();
+
+    void addLunch(LunchAddDTO lunchAddDTO);
+
+    LunchEditDTO getLunchById(Long id);
+
+    LunchEditDTO getLunchEditDtoById(Long id);
+
+    void saveLunch(LunchEditDTO lunchEditDTO);
+
+}
