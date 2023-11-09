7ca434f144a4fdccae45bacdf5abffd9e708b27d
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-09 20:13:13 +0200

    Update SecurityConfiguration.java

--- The Restaurant/src/main/java/com/example/demo_project/config/SecurityConfiguration.java
+++ The Restaurant/src/main/java/com/example/demo_project/config/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration {
                         // All static resources which are situated in js, images, css are available for anyone
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         // Allow anyone to see the home page, the registration page and the login form
-                        .requestMatchers("/","/menu","/drinks","/users/login", "/users/register", "/users/login-error").permitAll()
+                        .requestMatchers("/","/menu","/gallery","/users/login", "/users/register", "/users/login-error").permitAll()
                         // all other requests are authenticated.
                         .anyRequest().authenticated()
         ).formLogin(
