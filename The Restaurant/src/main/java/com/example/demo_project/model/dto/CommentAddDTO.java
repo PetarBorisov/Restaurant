a7fae90cf155eecbf44a4f487e4c07051c979ba6
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-15 01:33:29 +0200

    Create CommentAddDTO.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/model/dto/CommentAddDTO.java
@@ -0,0 +1,44 @@
+package com.example.demo_project.model.dto;
+
+public class CommentAddDTO {
+
+
+
+    private Long id;
+
+    private Long userId;
+    private String text;
+    private String userEmail;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
