de3ac5d24eaf790eed9130bfc5aa9ea19c280773
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-15 01:33:41 +0200

    Create CommentService.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.example.demo_project.service;
+
+import com.example.demo_project.model.dto.CommentAddDTO;
+import com.example.demo_project.model.entity.Comment;
+
+import java.util.List;
+
+
+public interface CommentService {
+
+   Comment addComment(CommentAddDTO commentAddDTO);
+    List<Comment> getComments();
+    void deleteComment(Long id);
+}
