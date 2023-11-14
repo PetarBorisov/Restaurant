4ccaf40c2a745ee77d96621f5c9fda3bc99dcf50
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-15 01:33:33 +0200

    Create Comment.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/model/entity/Comment.java
@@ -0,0 +1,46 @@
+package com.example.demo_project.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Entity
+@Table(name = "comments")
+public class Comment extends BaseEntity {
+
+    @Column(columnDefinition = "TEXT")
+   private String text;
+
+    private String userEmail;
+
+    @ManyToOne
+    private UserEntity user;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+}
