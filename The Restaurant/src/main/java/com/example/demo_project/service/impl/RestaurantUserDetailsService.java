b6fda10bd7228004ee66fa5e339a3a263595ea2a
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2023-11-22 01:50:03 +0200

    Create RestaurantUserDetailsService.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/service/impl/RestaurantUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.example.demo_project.service.impl;
+
+import com.example.demo_project.model.entity.UserEntity;
+import com.example.demo_project.model.entity.UserRoleEntity;
+import com.example.demo_project.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+
+public class RestaurantUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public RestaurantUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(email)
+                .map(RestaurantUserDetailsService::map)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + email + " not found!"));
+    }
+    private static UserDetails map(UserEntity userEntity) {
+        return User
+                .withUsername(userEntity.getEmail())
+                .password(userEntity.getPassword())
+                .authorities(userEntity.getRoles().stream().map(RestaurantUserDetailsService::map).toList())
+                .build();
+    }
+    private static GrantedAuthority map(UserRoleEntity userRoleEntity) {
+        return new SimpleGrantedAuthority(
+                "ROLE_" + userRoleEntity.getRole().name()
+        );
+    }
+}
