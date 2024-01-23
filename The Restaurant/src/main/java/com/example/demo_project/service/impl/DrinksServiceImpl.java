3339c67c620883d2c1043c8dbce39516c0177de3
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-01-24 00:26:20 +0200

    Update DrinksServiceImpl.java

--- The Restaurant/src/main/java/com/example/demo_project/service/impl/DrinksServiceImpl.java
+++ The Restaurant/src/main/java/com/example/demo_project/service/impl/DrinksServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo_project.service.impl;
 import com.example.demo_project.model.dto.DrinkEditDto;
 import com.example.demo_project.model.dto.DrinksAddDTO;
 import com.example.demo_project.model.entity.DrinkEntity;
+import com.example.demo_project.model.entity.LunchEntity;
 import com.example.demo_project.repository.DrinksRepository;
 import com.example.demo_project.service.DrinksService;
 import org.modelmapper.ModelMapper;
@@ -88,6 +89,27 @@ public class DrinksServiceImpl implements DrinksService {
         Optional<DrinkEntity> drinkEntityOptional = drinksRepository.findById(id);
         return drinkEntityOptional.map(drinkEntity -> modelMapper.map(drinkEntity, DrinkEditDto.class)).orElse(null);
 
+    }
+
+    @Override
+    public void editDrink(Long id, DrinkEditDto drinkEditDto) {
+        Optional<DrinkEntity> drinks = this.drinksRepository.findById(drinkEditDto.getId());
+
+        if (drinks.isPresent()) {
+            DrinkEntity existingDrink = drinks.get();
+            existingDrink.setName(drinkEditDto.getName());
+            existingDrink.setPhoto(drinkEditDto.getPhoto());
+            existingDrink.setDescription(drinkEditDto.getDescription());
+            existingDrink.setPrice(drinkEditDto.getPrice());
+
+            drinksRepository.save(existingDrink);
+        }
+    }
+
+    @Override
+    public void deleteDrink(Long id) {
+        Optional<DrinkEntity> delete = this.drinksRepository.findById(id);
+        delete.ifPresent(drinksRepository::delete);
     }
 
 }
