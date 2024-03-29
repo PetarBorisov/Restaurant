aa097c315e230c106f7a4c4713abbc64ddc0d721
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-03-30 00:38:57 +0200

    Create I18NConfig.java

---
+++ The Restaurant/src/main/java/com/example/demo_project/config/I18NConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo_project.config;
+
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class I18NConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setCookieName("lang");
+
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("i18n/messages");
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8"); // Задаване на UTF-8 кодировка
+        resourceBundleMessageSource.setUseCodeAsDefaultMessage(true); // Задаване на стойност true за избягване на празни съобщения
+        return resourceBundleMessageSource;
+
+    }
+}
+
