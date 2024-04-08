236b9ce341b0d44e5974eec54e4d1d2e767e83d1
PetarBorisov <108021054+PetarBorisov@users.noreply.github.com>
2024-04-09 00:40:44 +0300

    Update EmailService.java
    Create Interface for MailSender

--- The Restaurant/src/main/java/com/example/demo_project/service/EmailService.java
+++ The Restaurant/src/main/java/com/example/demo_project/service/EmailService.java
@@ -1,61 +1,16 @@
 package com.example.demo_project.service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.context.MessageSource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-
 import java.util.Locale;
 
-@Service
-public class EmailService {
+public interface EmailService {
 
-    private final TemplateEngine templateEngine;
-    private final JavaMailSender javaMailSender;
-    private final MessageSource messageSource;
+    void sendRegistrationEmail(String userEmail, String userName, Locale preferredLocale);
 
-    public EmailService(TemplateEngine templateEngine, JavaMailSender javaMailSender, MessageSource messageSource) {
-        this.templateEngine = templateEngine;
-        this.javaMailSender = javaMailSender;
-        this.messageSource = messageSource;
-    }
+    void sendReservationEmail(String userEmail, String userName, Locale preferredLocale);
 
-    public void sendRegistrationEmail(String userEmail, String userName, Locale preferredLocale) {
-        sendEmail(userEmail, getEmailSubject("registration_subject", preferredLocale),
-                generateMessageContent("email/registration", preferredLocale, userName));
-    }
+    void sendEmail(String userEmail, String subject, String content);
 
-    public void sendReservationEmail(String userEmail, String userName, Locale preferredLocale) {
-        sendEmail(userEmail, getEmailSubject("reservation_subject", preferredLocale),
-                generateMessageContent("email/success_reservation", preferredLocale, userName));
-    }
+    String getEmailSubject(String messageKey, Locale locale);
 
-    private void sendEmail(String userEmail, String subject, String content) {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-            mimeMessageHelper.setFrom("the@restaurant.com");
-            mimeMessageHelper.setTo(userEmail);
-            mimeMessageHelper.setSubject(subject);
-            mimeMessageHelper.setText(content, true);
-            javaMailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String getEmailSubject(String messageKey, Locale locale) {
-        return messageSource.getMessage(messageKey, new Object[0], locale);
-    }
-
-    private String generateMessageContent(String templateName, Locale locale, String userName) {
-        Context context = new Context();
-        context.setLocale(locale);
-        context.setVariable("userName", userName);
-        return templateEngine.process(templateName, context);
-    }
+    String generateMessageContent(String templateName, Locale locale, String userName);
 }
